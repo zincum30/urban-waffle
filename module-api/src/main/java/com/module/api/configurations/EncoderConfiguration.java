3229a19f9e748d8c48b7cad4b8d049fccf23cb40
practise30 <practise30@gmail.com>
2024-05-18 15:52:28 +0900

    fix : DelegatingPasswordEncoder 적용

--- module-api/src/main/java/com/module/api/configurations/EncoderConfiguration.java
+++ module-api/src/main/java/com/module/api/configurations/EncoderConfiguration.java
@@ -1,13 +1,34 @@
 package com.module.api.configurations;
 
+import com.module.api.encoder.DelegatingPasswordEncoder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.crypto.password.MessageDigestPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 @Configuration
 public class EncoderConfiguration {
 
     @Bean
-    public BCryptPasswordEncoder encoder(){return new BCryptPasswordEncoder();}
+    public static PasswordEncoder createDelegatingPasswordEncoder() {
+
+        String idForEncode = "bcrypt";
+
+        Map encoders = new HashMap<>();
+        encoders.put(idForEncode, new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder().defaultsForSpringSecurity_v5_8());
+        encoders.put("scrypt@SpringSecurity_v5_8", SCryptPasswordEncoder.defaultsForSpringSecurity_v5_8());
+
+        PasswordEncoder passwordEncoder = new DelegatingPasswordEncoder(idForEncode, encoders);
+        return passwordEncoder;
+    }
 }
